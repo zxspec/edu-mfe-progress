92be2f2faa76e03b57ff3cb85b9c281629f0977c
Dmytro Iievliev <dmytro.iievliev@adidas.com>
2021-09-26 23:35:56 +0200

    added appllication level css-prefixes

--- container/src/App.js
+++ container/src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import { BrowserRouter } from 'react-router-dom'
 import MarketingApp from './components/MarketingApp'
 import Header from './components/Header'
+import { createGenerateClassName, StylesProvider } from '@material-ui/core/styles'
 
-export default () => {
-    return <BrowserRouter>
-        <div>
-            <Header />
-            <MarketingApp />
-        </div>
-    </BrowserRouter>
-}
+const generateClassName = createGenerateClassName({ productionPrefix: "co" })
+
+export default () =>
+    <StylesProvider generateClassName={generateClassName}>
+        <BrowserRouter>
+            <div>
+                <Header />
+                <MarketingApp />
+            </div>
+        </BrowserRouter>
+    </StylesProvider>
